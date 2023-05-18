3525c41979c101215bb9aa436c9980afcaa47f26
rm86581 <yasmimferro06@gmail.com>
2023-05-18 08:26:17 -0300

    aula: orientação de objetos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Aula de orientação de objetos
+        // Declarar objetos
+        Pessoa adao;
+
+        // Instanciar objeto
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+
+        // Definição do formato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
+
+
+
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+    // Atributos em classe
+    public String nome;
+    public String sobrenome;
+
+    //Método da classe
+    public void falar () {
+        System.out.println("falei");
+    }
+}
