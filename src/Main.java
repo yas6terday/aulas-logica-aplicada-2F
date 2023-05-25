94776ff6c160430556024625e16ab52c6297a293
rm86581 <yasmimferro06@gmail.com>
2023-05-25 07:51:35 -0300

    aula: retomada classe objetos

--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,21 @@ public class Main {
         adao.falar();
         eva.falar();
 
+        // Criar mais objetos
+        Pessoa qualquer = new Pessoa();
 
+        // Declarar objeto
+        Pessoa rainha;
 
+        // Instância ou objeto
+        rainha = new Pessoa();
+
+        // Definindo forma da pessoa
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+
+        // Definição de comportamento
+        System.out.println(rainha.falar("alto"));
 
 
     }
