a5126c065e8a6139eaec561a19e4df5892c0fd34
rm86581 <yasmimferro06@gmail.com>
2023-06-01 08:22:17 -0300

    Atividade Balão

---
+++ src/Classes/Balao.java
@@ -0,0 +1,7 @@
+package Classes;
+
+public class Balao {
+
+    public int id;
+    public String reistro;
+}
---
+++ src/Classes/Piloto.java
@@ -0,0 +1,9 @@
+package Classes;
+
+public class Piloto {
+
+    public String nome;
+    public int CPF;
+    public int registroANAC;
+
+}
